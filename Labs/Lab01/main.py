45099e9caa0e6aeee608aef953793516e9733206
Miguel Martins <miguel.martinsrn@gmail.com>
2025-05-08 00:23:31 -0300

    Informações dos jogos

--- Labs/Lab01/main.py
+++ Labs/Lab01/main.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 # Configurações do Azure Storage
 CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=stadiolab01;AccountKey=w/JH5023cqwbbdYZLQ23PtU7sigjsCrEGgCBT0laN7jtpbnqP6JYVokocxDb7kjmN68khmDePLNo+AStvwz87g==;EndpointSuffix=core.windows.net"
-CONTAINER_NAME = "produtos"
+CONTAINER_NAME = "Jogos"
 ACCOUNT_NAME = "stadiolab01"
 
 # Configurações do Azure SQL Server
@@ -18,13 +18,13 @@ SQL_USERNAME = "adminhsouza"
 SQL_PASSWORD = "P@$$w0rd2022dev001hsouza"
 
 # Título da aplicação
-st.title("Cadastro de Produto - E-Commerce na Cloud")
+st.title("Cadastro - Virtual Games Store")
 
 # Formulário para cadastro do produto
-product_name = st.text_input("Nome do Produto")
-description = st.text_area("Descrição do Produto")
-price = st.number_input("Preço do Produto", min_value=0.0, format="%.2f")
-uploaded_file = st.file_uploader("Imagem do Produto", type=["png", "jpg", "jpeg"])
+product_name = st.text_input("Nome do Jogo")
+description = st.text_area("Descrição do Jogo")
+price = st.number_input("Preço do Jogo", min_value=0.0, format="%.2f")
+uploaded_file = st.file_uploader("Imagem do Jogo", type=["png", "jpg", "jpeg"])
 
 # Função para enviar imagem para o Azure Blob Storage
 def upload_image(file):
@@ -78,11 +78,11 @@ def list_products_sql():
         conn.close()
         return rows
     except Exception as e:
-        st.error(f"Erro ao listar produtos: {e}")
+        st.error(f"Erro ao listar jogos: {e}")
         return []
 
 # Função para exibir a lista de produtos na tela   
-def list_produtos_screen():
+def list_jogos_screen():
         products = list_products_sql()
         if products:
         # Define o número de cards por linha
@@ -103,11 +103,11 @@ def list_produtos_screen():
                 if (i + 1) % cards_por_linha == 0 and (i + 1) < len(products):
                     cols = st.columns(cards_por_linha)
         else:
-            st.info("Nenhum produto encontrado.")
+            st.info("Nenhum jogo encontrado.")
 
 
-# Botão para cadastro do produto
-if st.button("Cadastrar Produto"):
+# Botão para cadastro do jogo
+if st.button("Cadastrar Jogo"):
     if not product_name or not description or price is None:
         st.warning("Preencha todos os campos obrigatórios!")
     else:
@@ -126,13 +126,13 @@ if st.button("Cadastrar Produto"):
 
         # Insere os dados no Azure SQL Server
         if insert_product_sql(product_data):
-            st.success("Produto cadastrado com sucesso no Azure SQL!")
-            list_produtos_screen()
+            st.success("Jogo cadastrado com sucesso no Azure SQL!")
+            list_jogos_screen()
         else:
-            st.error("Houve um problema ao cadastrar o produto no Azure SQL.")
+            st.error("Houve um problema ao cadastrar o jogo no Azure SQL.")
 
         # Opcional: Salva os dados localmente (exemplo usando arquivo JSON)
-        file_path = "produtos.json"
+        file_path = "jogos.json"
         if os.path.exists(file_path):
             with open(file_path, "r", encoding="utf-8") as f:
                 try:
@@ -148,9 +148,9 @@ if st.button("Cadastrar Produto"):
         
         st.json(product_data)
 
-st.header("Listagem dos Produtos")
+st.header("Listagem dos Jogos")
 
 
-# Botão para carregar e exibir a lista de produtos
-if st.button("Listar Produtos"):
-    list_produtos_screen()
+# Botão para carregar e exibir a lista de jogos
+if st.button("Listar Jogos"):
+    list_jogos_screen()
